5391b185649ebdbba196c845c6ff74a6f6eef15c
lenoz <lenoz@thelenoz.me>
2025-05-16 15:02:10 +0300

    Handling subscribe command

--- handlers/sub_management_handler.py
+++ handlers/sub_management_handler.py
@@ -2,14 +2,14 @@ from aiogram import Router, types, F, Bot
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import any_state
-
+from aiogram.types import Message
 
 sub_management_router = Router()
 
 @sub_management_router.message(Command("subscribe"))
-async def handle_sub_command(call: types.CallbackQuery, state: FSMContext, bot: Bot):
-    await sub_management(call, state, bot)
+async def handle_sub_command(message: Message, bot: Bot):
+    await message.answer("Меню управления подпиской (ещё не готово)")
 
 @sub_management_router.callback_query(F.data.startswith("sub_management"), any_state)
 async def sub_management(call: types.CallbackQuery, state: FSMContext, bot: Bot):
-    await call.message.answer("Меню управления подпиской (ещё не готово)")
+    await handle_sub_command(call.message, bot)
