1b00a1489925baf29a9d8f9dd9c1c5e436402fc1
lenoz <lenoz@thelenoz.me>
2025-05-18 13:58:51 +0300

    checkup main menu

--- utils/checkups.py
+++ utils/checkups.py
@@ -34,6 +34,6 @@ async def send_checkup(checkup_id: int):
 
 async def is_ended_today(checkup_id: int) -> bool:
     day_checkup = await days_checkups_repository.get_latest_ended_day_checkup_by_checkup_id(checkup_id)
-    if day_checkup and day_checkup.date_end_day and day_checkup.date_end_day == datetime.now().date():
+    if day_checkup and day_checkup.date_end_day and day_checkup.date_end_day.date() == datetime.now().date():
         return True
     return False
