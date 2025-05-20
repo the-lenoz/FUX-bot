cc36fdc540217c5ef81bafe55803068314e258c6
lenoz <lenoz@thelenoz.me>
2025-05-20 19:54:50 +0300

    changed exercises shown log

--- handlers/exercises_handler.py
+++ handlers/exercises_handler.py
@@ -1,30 +1,23 @@
-import asyncio
-import datetime
+import logging
 
-from aiogram import Router, F, types, Bot
+from aiogram import Router, F, Bot
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import any_state
-from aiogram.types import Message, CallbackQuery, InputFile, BufferedInputFile, InlineKeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import CallbackQuery
 
-from data.keyboards import menu_keyboard, fast_help_keyboard, mental_helper_keyboard, menu_button, go_deeper_keyboard
-from db.models import ExercisesUser
-from db.repository import fast_help_repository, fast_help_dialog_repository, \
-    go_deeper_repository, go_deeper_dialogs_repository, operation_repository, summary_user_repository, \
-    mental_problems_repository, exercises_user_repository, users_repository
-from settings import InputMessage, mechanic_text, mechanic_checkup, is_valid_email, fast_help_promt, go_deeper_promt, \
-    mechanic_dict, exercises_photo
+from data.keyboards import menu_keyboard
+from db.repository import users_repository
+from settings import mechanic_dict, exercises_photo
 from utils.gpt_distributor import user_request_handler
-from utils.gpt_generate_recommendations import generate_recommendations, generate_summary, generate_exercises, \
-    generate_feedback_exercises
-
 
 exercises_router = Router()
+logger = logging.getLogger(__name__)
 
 @exercises_router.callback_query(F.data == "exercises_by_problem")
 async def exercises_by_problem_call(call: CallbackQuery, state: FSMContext, bot: Bot):
     user_id = call.from_user.id
     user = await users_repository.get_user_by_user_id(user_id)
+    logger.info(f"User {user_id} used exercises value: {user.used_exercises},"
+                f" casted to bool: {False if not user.used_exercises else True}")
     if not user.used_exercises:
         await call.message.answer_photo(caption=mechanic_dict.get("exercises_by_problem"),
                                         photo=exercises_photo)
