744bf1e1a12e113fe682b55c6e8462a487631a27
lenoz <lenoz@thelenoz.me>
2025-06-21 09:48:40 +0300

    Fixed check_is_full_registered

--- utils/subscription.py
+++ utils/subscription.py
@@ -3,7 +3,7 @@ from db.repository import users_repository, subscriptions_repository
 
 async def check_is_subscribed(user_id: int) -> bool:
     user = await users_repository.get_user_by_user_id(user_id=user_id)
-    if user is not None and user.full_registration:
+    if user is not None:
         user_sub = await subscriptions_repository.get_active_subscription_by_user_id(user_id=user_id)
         if user_sub is not None:
             return True
