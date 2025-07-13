9acfdfba52e1363c6dd47b94600710e82545e589
lenoz <lenoz@thelenoz.me>
2025-07-13 18:20:14 +0300

    Текст трекингов

--- handlers/system_settings_handler.py
+++ handlers/system_settings_handler.py
@@ -52,6 +52,7 @@ async def send_system_settings(user_id: int):
         InlineKeyboardButton(text=f"Пол: {'Мужской' if user.gender == 'male' else ('Женский' if user.gender == 'female' else 'НЕ УСТАНОВЛЕН')}",
                              callback_data="settings|edit|gender")
     )
+    keyboard.row(InlineKeyboardButton(text=f"👄Режим общения: {'Прямолинейный' if user.ai_temperature == 0.6 else 'Нейтральный'}", callback_data="settings|temperature"))
     keyboard.row(
         InlineKeyboardButton(
             text=f"Часовой пояс: {user_timezone_name if user_timezone_name else 'НЕ УСТАНОВЛЕН'}",
@@ -59,7 +60,6 @@ async def send_system_settings(user_id: int):
     )
     if user.email:
         keyboard.row(InlineKeyboardButton(text=f"Email: {user.email}", callback_data="settings|edit|email"))
-    keyboard.row(InlineKeyboardButton(text=f"Режим общения: {'Прямолинейный' if user.ai_temperature == 0.6 else 'Нейтральный'}", callback_data="settings|temperature"))
     for checkup in user_checkups:
         text = ("Время трекинга эмоций🤩" if checkup.type_checkup == "emotions" else "Время трекинга продуктивности🚀") + f": {(datetime.combine(datetime.today(), checkup.time_checkup) + timezone_delta).time().strftime('%H:%M')}"
         keyboard.row(InlineKeyboardButton(text=text, callback_data=f"edit_checkup|{checkup.id}"))
