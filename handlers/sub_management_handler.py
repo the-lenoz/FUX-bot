fc44d508849806c5b5cd6491739d3a62ea7c9d09
lenoz <lenoz@thelenoz.me>
2025-07-25 19:12:37 +0300

    Текст

--- handlers/sub_management_handler.py
+++ handlers/sub_management_handler.py
@@ -17,13 +17,14 @@ async def handle_sub_command(message: Message, bot: Bot):
 
 @sub_management_router.callback_query(F.data.startswith("sub_management"), any_state)
 async def sub_management(call: types.CallbackQuery, state: FSMContext, bot: Bot):
+    await call.message.delete()
     await subscription_management_menu(call.from_user.id)
 
 
 async def subscription_management_menu(user_id: int):
     if await check_is_subscribed(user_id):
         await main_bot.send_photo(user_id,
-                                  caption=messages_dict["buy_sub_photo_description"],
+                                  caption=messages_dict["your_sub_photo_description"],
                                   photo=premium_sub_photo,
                                   reply_markup=generate_sub_keyboard().as_markup())
     else:
