7b352a0095425b9ad0182b2226b9239455b6a893
lenoz <lenoz@thelenoz.me>
2025-07-30 22:04:19 +0300

    Новая статистика

--- utils/statistics.py
+++ utils/statistics.py
@@ -34,53 +34,82 @@ class MainStatistics(BaseModel):
 
     active_trackings: int = 0
 
-    top_3_active_users: List[str]
+    top_3_users_str: str = ""
 
     all_messages: int = 0
 
     @staticmethod
     async def generate():
         now_date = datetime.datetime.now(datetime.timezone.utc).replace(tzinfo=None)
-        users_count_statistic = await users_repository.get_user_creation_statistics()
-        messages_count_statistic = await ai_requests_repository.get_ai_requests_statistics()
 
         operations = await operation_repository.select_all_operations()
         events = await events_repository.select_all_events()
         active_trackings = await checkup_repository.select_all_active_checkups()
+        users = await users_repository.select_all_users()
 
         payments_per_day: int = 0
         payments_per_week: int = 0
+        payments_per_month: int = 0
         all_payments: int = 0
 
-        for operation in sorted(operations, key=lambda op: op.upd_date, reverse=True):
+        for operation in operations:
             if operation.is_paid:
-                if now_date - operation.upd_date.replace(tzinfo=None) < datetime.timedelta(hours=24):
+                if now_date.date() == operation.creation_date.date():
                     payments_per_day += 1
-                if now_date - operation.upd_date.replace(tzinfo=None) < datetime.timedelta(days=7):
+                if (now_date.isocalendar().week == operation.creation_date.isocalendar().week
+                        and now_date.isocalendar().year == operation.creation_date.isocalendar().year):
                     payments_per_week += 1
+                if now_date.month == operation.creation_date.month and now_date.year == operation.creation_date.year:
+                    payments_per_month += 1
                 all_payments += 1
+
+        new_users_per_day: int = 0
+        new_users_per_week: int = 0
+        new_users_per_month: int = 0
+        all_users = 0
+
+        for user in users:
+            if now_date.date() == user.creation_date.date():
+                new_users_per_day += 1
+            if (now_date.isocalendar().week == user.creation_date.isocalendar().week
+                    and now_date.isocalendar().year == user.creation_date.isocalendar().year):
+                new_users_per_week += 1
+            if now_date.month == user.creation_date.month and now_date.year == user.creation_date.year:
+                new_users_per_month += 1
+            all_users += 1
 
         current_day_active_users_set = set()
         current_week_active_users_set = set()
+        current_month_active_users_set = set()
         for event in events:
-            if now_date - event.creation_date.replace(tzinfo=None) < datetime.timedelta(hours=24):
+            if now_date.date() == event.creation_date.date():
                 current_day_active_users_set.add(event.user_id)
-            if now_date - event.creation_date.replace(tzinfo=None) < datetime.timedelta(days=7):
+            if (now_date.isocalendar().week == event.creation_date.isocalendar().week
+                    and now_date.isocalendar().year == event.creation_date.isocalendar().year):
                 current_week_active_users_set.add(event.user_id)
+            if now_date.month == event.creation_date.month and now_date.year == event.creation_date.year:
+                current_month_active_users_set.add(event.user_id)
 
         return MainStatistics(
-            new_users_per_day=users_count_statistic.get('day'),
-            new_users_per_week=users_count_statistic.get('week'),
-            all_users=users_count_statistic.get('all_time'),
-            payments_per_day=payments_per_day,
-            payments_per_week=payments_per_week,
-            all_payments=all_payments,
-            new_messages_per_day=messages_count_statistic.get('day').get('total'),
-            new_messages_per_week=messages_count_statistic.get('week').get('total'),
-            all_messages=messages_count_statistic.get('all_time').get('total'),
+            new_users_per_day=new_users_per_day,
+            new_users_per_week=new_users_per_week,
+            new_users_per_month=new_users_per_month,
+            all_users=all_users,
             current_day_active_users=len(current_day_active_users_set),
             current_week_active_users=len(current_week_active_users_set),
-            active_trackings=len(active_trackings)
+            current_month_active_users=len(current_week_active_users_set),
+            payments_per_day=payments_per_day,
+            payments_per_week=payments_per_week,
+            payments_per_month=payments_per_month,
+            all_payments=all_payments,
+            new_messages_per_day=0,
+            new_messages_per_week=0,
+            promocodes_per_day=0,
+            promocodes_per_week=0,
+            promocodes_per_month=0,
+            active_trackings=len(active_trackings),
+            top_3_users_str="",
+            all_messages=0
         )
 
 async def generate_statistics_text():
