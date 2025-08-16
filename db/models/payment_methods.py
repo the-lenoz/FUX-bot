1be4aac5db8bcd9fd97d5705a7caf5f1e001d005
lenoz <lenoz@thelenoz.me>
2025-08-16 14:40:35 +0300

    - Сохранение метода оплаты - fix

--- db/models/payment_methods.py
+++ db/models/payment_methods.py
@@ -4,5 +4,7 @@ from db.base import CleanModel, BaseModel
 
 
 class PaymentMethod(BaseModel, CleanModel):
+    __tablename__ = 'payment_methods'
+
     user_id = Column(BigInteger, nullable=False)
     payment_method_id = Column(String)
